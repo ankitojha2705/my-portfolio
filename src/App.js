536b78032887feba87e6668b8f8cc78ffd500e18
ankitojha2705 <ankit2705ojha@gmail.com>
2025-08-19 15:22:28 -0700

    production fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+// In src/App.js
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 import Skills from './components/Skills';
 import Education from './components/Education';
 import Experience from './components/Experience';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <About />
-      <Skills />
-      <Education />
-      <Experience />
-      <Projects />
-      <Contact />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <About />
+            <Skills />
+            <Education />
+            <Experience />
+            <Projects />
+            <Contact />
+          </>
+        } />
+        {/* Add more routes here if needed */}
+      </Routes>
       <Footer />
     </div>
   );
